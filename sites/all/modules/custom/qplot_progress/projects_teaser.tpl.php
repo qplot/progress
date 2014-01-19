68f1378daa97d802949a50057cf1d6c49c952f92
Fang Jin <windmaomao@gmail.com>
2014-01-19 16:31:56 -0500

    add grid panel layout to theme

--- sites/all/modules/custom/qplot_progress/projects_teaser.tpl.php
+++ sites/all/modules/custom/qplot_progress/projects_teaser.tpl.php
@@ -1,10 +1,7 @@
 <?php 
 	global $base_url;
 	$path = $base_url . '/' . drupal_get_path('theme', 'webarch');
-?>
-
-<?php 
-	dsm($items);
+  // dsm($items);
 ?>
 
 <div class="side-bar-widgets">
@@ -12,18 +9,18 @@
     PROJECTS
   </p>
   <?php foreach($items as $project): ?>
-  <?php $bar = ($project['status'] == 'Active') ? 'success' : 'failure'; ?>
-  <div class="status-widget">
-    <div class="status-widget-wrapper">
-      <span class="pull-right"><i class="fa fa-<?=$project['icon'] ?>"></i></span>
-      <div class="title">
-        <?=$project['title'] ?>
-      </div>
-      <?=$project['description']  ?>
-      <div class="progress transparent progress-small no-radius no-margin">
-        <div data-percentage="<?=$project['progress'] ?>%" class="progress-bar progress-bar-<?=$bar ?> animate-progress-bar"></div>
+    <?php $bar = ($project['status'] == 'Active') ? 'success' : 'failure'; ?>
+    <div class="status-widget">
+      <div class="status-widget-wrapper">
+        <span class="pull-right"><i class="fa fa-<?=$project['icon'] ?>"></i></span>
+        <div class="title">
+          <?=$project['title'] ?>
+        </div>
+        <?=$project['description']  ?>
+        <div class="progress transparent progress-small no-radius no-margin">
+          <div data-percentage="<?=$project['progress'] ?>%" class="progress-bar progress-bar-<?=$bar ?> animate-progress-bar"></div>
+        </div>
       </div>
     </div>
-  </div>
-<?php endforeach; ?>
+  <?php endforeach; ?>
 </div>
