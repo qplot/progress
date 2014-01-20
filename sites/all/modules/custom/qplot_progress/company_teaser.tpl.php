43314f497b808e11d9c2b7936bab91defdbfff9c
Zach Burk <zburk@qplot.com>
2014-01-20 10:25:56 -0500

    refine company teaser template to include company info

--- sites/all/modules/custom/qplot_progress/company_teaser.tpl.php
+++ sites/all/modules/custom/qplot_progress/company_teaser.tpl.php
@@ -1,17 +1,22 @@
 <?php 
 	global $base_url;
 	$path = $base_url . '/' . drupal_get_path('theme', 'webarch');
- ?>
+
+//  dsm($company);
+?>
 <div class="user-info-wrapper">
-  <img src="<?=$path ?>/assets/img/logos/alphamed_press.png" alt="" data-src="<?=$path ?>/assets/img/logos/alphamed_press.png" data-src-retina="assets/img/profiles/avatar2x.jpg" height="36px">
-  <div class="user-info">
-	<div class="greeting"></div>
-	<div class="username">
-	  AlphaMed Press
-	</div>
-	<div class="status">
-	  Status<a href="#"></a>
-	  <div class="status-icon green"></div>Active
-	</div>
-  </div>
+    <img src="%3C?php%20echo%20$company['logo']%20?%3E" alt="" data-src="<?php echo $company['logo'] ?>" data-src-retina="assets/img/profiles/avatar2x.jpg" height="36px">
+    <div class="user-info">
+        <div class="greeting"></div>
+        <div class="username">
+            <?php echo $company['title'] ?>
+        </div>
+        <div class="status">
+            Status<a href="#"></a>
+            <div class="status-icon green"></div><?php echo $company['status'] ?>
+        </div>
+        <?php if ($company['edit']): ?>
+          <a href="<?php echo $company['edit'] ?>" class="btn btn-primary btn-xs btn-mini"><i class="fa fa-edit"></i></a>
+        <?php endif; ?>        
+    </div>
 </div>
