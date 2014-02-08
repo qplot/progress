9f43296314325ddad0e4bdbd0e585ce21c79cc45
Fang Jin <windmaomao@gmail.com>
2014-02-08 16:18:37 -0500

    apply theme to edit button in views

--- sites/all/themes/webarch/functions/views.php
+++ sites/all/themes/webarch/functions/views.php
@@ -14,7 +14,6 @@ template_preprocess_views_view
 options to remove css classes from the view
 */
 function webarch_preprocess_views_view(&$vars) {
-  dsm($vars);
   $vars['classes_array'] = array_values(array_diff($vars['classes_array'],array('view')));
   $vars['classes_array'] = array_values(array_diff($vars['classes_array'],array('view-'.$vars['name'])));
   $vars['classes_array'] = array_values(array_diff($vars['classes_array'],array('view-id-'.$vars['name'])));
@@ -22,11 +21,29 @@ function webarch_preprocess_views_view(&$vars) {
 }
 
 /**
- * theme table
+ * Theme table
  */
 function webarch_preprocess_views_view_table(&$vars) {
+  // dsm($vars);
   if (!empty($vars['classes_array'])) {
     // replace with custom class
     $vars['classes_array'][0] = 'table';
   }
 }
+
+/**
+ * Theme table field
+ */
+function webarch_preprocess_views_view_field(&$vars) {
+  if (get_class($vars['field']) == 'views_handler_field_node_link_edit') {
+    // dsm($vars['row']);
+    // Rerender the link to include edit button
+    $vars['output'] = l('<i class="fa fa-edit"></i>', "node/{$vars['row']->nid}/edit", array(
+      'attributes' => array('title' => 'edit'), 
+      'query' => drupal_get_destination(),
+      'html' => TRUE,
+    ));
+  }
+ // $vars['output'] = $vars['field']->advanced_render($vars['row']);
+}
+
