7eb36ebe9cc8efdac0f15acdf7ada15de01c549f
Fang Jin <windmaomao@gmail.com>
2014-03-01 22:34:24 -0500

    add error theme handling

---
+++ sites/all/modules/custom/qplot_progress/templates/error_page.tpl.php
@@ -0,0 +1,12 @@
+<div class="row">
+    <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-offset-1 col-xs-10">
+   <div class="error-container" >
+    <div class="error-main">
+      <div class="error-number"> <?php echo $error ?> </div>
+      <div class="error-description" > <?php echo $description ?> </div>
+      <div class="error-description-mini"> <?php echo $message ?> </div>
+      <br>
+    </div>
+    </div>
+  </div>
+</div>
